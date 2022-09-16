8aa70d601b6d5edbd9018daad813c35ad6f990e4
Willy Brauner <mail@willybrauner.com>
2022-09-16 15:27:15 +0200

    Prepare new instance system

--- src/Timeline.ts
+++ src/Timeline.ts
@@ -5,19 +5,7 @@ interface ITimelineConstruct {
   paused?: boolean
 }
 
-interface IAdd {
-  interpol: Interpol
-  offsetDuration: number
-  // Start position of this add inside the full timeline
-  startPositionInTl: number
-  // position progress (where we are) inside this current add
-  position: number
-  // play state of this add
-  play: boolean
-}
-
 export class Timeline {
-
   protected _isPaused = false
   public get isPaused() {
     return this._isPaused
@@ -28,12 +16,10 @@ export class Timeline {
     return this._isPlaying
   }
 
-
-  protected adds: IAdd[] = []
-  protected timeline = []
+  protected adds: Add[] = []
+  // protected timeline = []
   protected onCompleteDeferred = deferredPromise()
   protected timeouts: ReturnType<typeof setTimeout>[] = []
-
 
   constructor({ paused = true }: ITimelineConstruct = {}) {
     this._isPaused = paused
@@ -44,17 +30,33 @@ export class Timeline {
    *
    *
    */
-  add(interpol: Interpol, offsetDuration: number = 0) {
-    this.adds.push({
-      interpol,
-      offsetDuration,
-      startPositionInTl: 0,
-      position: 0,
-      play: false,
-    })
+  add(interpol: Interpol, offsetPosition: number = 0) {
+    const lastAdd = this.adds[this.adds.length - 1]
+    console.log(">", this.adds)
+
+    // if lastAdd exist, calc start position of this new Add, else, this is the first one
+    const startPositionInTl = lastAdd
+      ? lastAdd.startPositionInTl + interpol.duration + offsetPosition
+      : 0
+
+    // update all "isLastOfTl property
+    for (let i = 0; i < this.adds.length; i++) {
+      this.adds[i].isLastOfTl = false
+    }
+
+    // push new Add instance in local
+    this.adds.push(
+      new Add({
+        interpol,
+        offsetPosition,
+        startPositionInTl,
+        isLastOfTl: true,
+      })
+    )
+
+    // return all the instance allow chaining methods calls
     return this
   }
-
 
   async play() {
     if (this.isPlaying) {
@@ -64,13 +66,13 @@ export class Timeline {
 
     const durations: number[] = []
     for (let i = 0; i < this.adds.length; i++) {
-      const prev: IAdd = this.adds[i - 1]
-      const curr: IAdd = this.adds[i]
+      const prev: Add = this.adds[i - 1]
+      const curr: Add = this.adds[i]
       const isLast = i === this.adds.length - 1
 
       // TODO faire ce calc dans add pour avoir directement les valeurs dispo
       // add new duration
-      durations.push((prev?.interpol.duration || 0) + curr.offsetDuration)
+      durations.push((prev?.interpol.duration || 0) + curr.offsetPosition)
       console.log("durations after push", durations)
 
       const pos = durations.reduce((a, b) => a + b)
@@ -109,4 +111,53 @@ export class Timeline {
   }
 }
 
+/**
+ *
+ * Add
+ * a single Tl entry
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
 
+class Add {
+  // interpol instance added to the timeline
+  public interpol: Interpol
+  // offset of position
+  public offsetPosition: number = 0
+  // Start position of this add inside the full timeline
+  public startPositionInTl: number = 0
+  // is last "Add" instance of the parent TL
+  public isLastOfTl: boolean = true
+  // play state of this add
+  public play: boolean = false
+  // position progress (where we are) inside this current add
+  public position: number
+
+  constructor({
+    interpol,
+    offsetPosition,
+    startPositionInTl,
+    isLastOfTl,
+    play,
+    position,
+  }: {
+    interpol: Interpol
+    offsetPosition: number
+    startPositionInTl: number
+    isLastOfTl: boolean
+    play?: boolean
+    position?: number
+  }) {
+    this.interpol = interpol
+    this.offsetPosition = offsetPosition
+    this.startPositionInTl = startPositionInTl
+    this.isLastOfTl = isLastOfTl
+    this.play = play
+    this.position = position
+  }
+}
