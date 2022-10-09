0c7ceca374ece46c89c32f0144f4d092c4f16b3d
Willy Brauner <mail@willybrauner.com>
2022-10-09 19:32:36 +0200

    WIP API

---
+++ examples/animate-dom-basic/src/App.tsx
@@ -0,0 +1,30 @@
+import "./App.css"
+import React, { useEffect, useRef, useState } from "react"
+import { Interpol, Ease, Interdom } from "../../../src"
+import { Controls } from "./Controls"
+import { randomRange } from "../../../test/utils/randomRange"
+
+export function App() {
+  const $ball = useRef<HTMLDivElement>()
+  const [instance, setInstance] = useState(null)
+  useEffect(() => {
+    const i = Interdom($ball.current, {
+      duration: 1000,
+      repeatRefresh: true,
+      left: () => randomRange(0, innerWidth / 2),
+      top: () => randomRange(0, innerHeight / 2),
+      ease: Ease.outCubic,
+
+    })
+    setInstance(i)
+  }, [])
+
+  return (
+    <div className={"App"}>
+      <Controls instance={instance} />
+      <div className={"wrapper"}>
+        <div className={"ball"} ref={$ball} />
+      </div>
+    </div>
+  )
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Timeline } from "./Timeline"
 export { Interpol } from "./Interpol"
 export { Ease } from "./Ease"
+export { Interdom } from "./Animate"
