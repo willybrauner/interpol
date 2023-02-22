09a664b0fcd0df9297b96ee2c5bed04797e91c51
Willy Brauner <mail@willybrauner.com>
2023-02-22 09:30:42 +0100

    Manage from to

--- examples/itp-basic/src/App.tsx
+++ examples/itp-basic/src/App.tsx
@@ -2,30 +2,23 @@ import "./App.css"
 import React, { useEffect, useRef, useState } from "react"
 import { Ease, itp } from "@wbe/interpol"
 import { Controls } from "./Controls"
-import { randomRange } from "../../../test/utils/randomRange"
 
 export function App() {
   const $ball = useRef<HTMLDivElement>()
   const [instance, setInstance] = useState(null)
   /**
-
    itp.from(el, {})
    itp.to(el, {})
    itp.fromTo(el, {})
-
    */
+
   useEffect(() => {
     const i = itp($ball.current, {
       duration: 1000,
-      left: [-100, 300],
-      top: [100, 300],
+      //      left: [-100, undefined],
+      left: 200,
+      top: [-100, 300],
       ease: Ease.outCubic,
-      beforeStart: () => {
-        //    console.log("itp beforeStart")
-      },
-      onUpdate: (e) => {
-        //      console.log("itp update",e)
-      },
     })
     setInstance(i)
   }, [])
