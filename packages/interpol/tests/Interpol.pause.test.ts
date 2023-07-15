dbf3136d3b27c2b70a0261ec92ba63a60b0a2c2f
willybrauner <mail@willybrauner.com>
2023-07-15 18:36:24 +0200

    Use mock in Interpol.pause.test

--- packages/interpol/tests/Interpol.pause.test.ts
+++ packages/interpol/tests/Interpol.pause.test.ts
@@ -5,7 +5,7 @@ import { Interpol } from "../src"
 describe.concurrent("Interpol pause", () => {
   it("should play, pause and play again (resume)", async () => {
     const mock = vi.fn()
-    let savedTime
+    let savedTime = vi.fn(() => 0)
     return new Promise(async (resolve: any) => {
       const itp = new Interpol({
         to: 1000,
@@ -21,12 +21,12 @@ describe.concurrent("Interpol pause", () => {
       expect(mock).toHaveBeenCalled()
       expect(itp.isPlaying).toBe(false)
       // save time before restart (should be around 500)
-      savedTime = itp.time
+      savedTime.mockReturnValue(itp.time)
       // and play again (resume)
       itp.play()
       // We are sure that time is not reset on play() after pause()
       await wait(100)
-      expect(itp.progress - savedTime).toBeLessThan(150)
+      expect(itp.progress - savedTime()).toBeLessThan(150)
       expect(itp.isPlaying).toBe(true)
       resolve()
     })
