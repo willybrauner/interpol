a23100c1931427e740449eb5e4329d3e83a4b639
Willy Brauner <willybrauner@gmail.com>
2023-07-23 21:38:43 +0200

    Clean example

--- examples/interpol-timeline/src/main.ts
+++ examples/interpol-timeline/src/main.ts
@@ -12,10 +12,8 @@ const inputSlider = document.querySelector<HTMLInputElement>(".slider")
 inputSlider.oninput = () => {
   tl.seek(parseFloat(inputSlider.value) / 100)
 }
-
 document.querySelector<HTMLButtonElement>(`.play`).onclick = () => tl.play()
 document.querySelector<HTMLButtonElement>(`.reverse`).onclick = () => tl.reverse()
-
 const ball = document.querySelector<HTMLElement>(".ball")
 
 /**
@@ -24,33 +22,8 @@ const ball = document.querySelector<HTMLElement>(".ball")
  *
  *
  */
-
-type Axis = [number, string?] | number
-type Axis3d = [Axis, Axis, Axis]
-const t3d = (dX: Axis = [0, "px"], dY: Axis = [0, "px"], dZ: Axis = [0, "px"]) => {
-  //  const initial: Axis3d = [dX, dY, dZ]
-  const current: Axis3d = [dX, dY, dZ]
-  const toString = (c: Axis3d) => {
-    return `translate3d(${c.reduce(
-      (acc, axis, i) => acc + `${axis[0]}${axis[1]}` + (i !== c.length - 1 ? ", " : ""),
-      ""
-    )})`
-  }
-  return toString(current)
-
-  // return {
-  //   set: ({ x = current[0], y = current[1], z = current[2] }: { x?: Axis; y?: Axis; z?: Axis }) => {
-  //     current[0][0] = x?.[0] ?? x ?? 0
-  //     current[0][1] = x?.[1] || "px"
-  //     current[1][0] = y?.[0] ?? y ?? 0
-  //     current[1][1] = y?.[1] || "px"
-  //     current[2][0] = z?.[0] ?? z ?? 0
-  //     current[2][1] = z?.[1] || "px"
-  //     return toString(current)
-  //   },
-  //   get: () => toString(current),
-  //   reset: () => (current = initial),
-  // }
+const styles = (el: HTMLElement | null, s: Record<string, string>) => {
+  for (let key in s) if (s.hasOwnProperty(key)) el.style[key] = s[key]
 }
 
 /**
@@ -59,8 +32,6 @@ const t3d = (dX: Axis = [0, "px"], dY: Axis = [0, "px"], dZ: Axis = [0, "px"]) =
  *
  *
  */
-const ballTranslate3d = t3d()
-
 const tl: Timeline = new Timeline({
   debug: true,
   paused: true,
@@ -75,7 +46,9 @@ const itp = new Interpol<"y" | "x">({
   duration: 500,
   ease: Power1.in,
   onUpdate: ({ props: { x, y } }) => {
-    ball.style.transform = `translate3d(${x}px, ${y}px, 0)`
+    styles(ball, {
+      transform: `translate3d(${x}px, ${y}px, 0)`,
+    })
   },
 
   onComplete: (e) => {
@@ -93,7 +66,9 @@ tl.add({
   duration: 500,
   ease: Power1.out,
   onUpdate: ({ props: { x, y } }) => {
-    ball.style.transform = `translate3d(${x}px, ${y}px, 0)`
+    styles(ball, {
+      transform: `translate3d(${x}px, ${y}px, 0)`,
+    })
   },
   onComplete: (e) => {
     console.log("itp 2 onComplete", e)
@@ -108,7 +83,9 @@ tl.add({
   duration: 500,
   ease: Power1.out,
   onUpdate: ({ props: { x, y } }) => {
-    ball.style.transform = `translate3d(${x}px, ${y}px, 0)`
+    styles(ball, {
+      transform: `translate3d(${x}px, ${y}px, 0)`,
+    })
   },
   onComplete: (e) => {
     console.log("itp 3 onComplete", e)
