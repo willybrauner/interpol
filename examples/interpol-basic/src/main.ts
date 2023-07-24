ea344620e56c7ce8ccfbe3962995de8cd1678ea2
Willy Brauner <willybrauner@gmail.com>
2023-07-24 23:28:04 +0200

    Update example

--- examples/interpol-basic/src/main.ts
+++ examples/interpol-basic/src/main.ts
@@ -1,14 +1,22 @@
 import { Interpol } from "@wbe/interpol"
 import "./index.less"
 ;["play", "reverse", "pause", "stop", "refresh", "resume"].forEach(
-  (name) => (document.querySelector<HTMLButtonElement>(`.${name}`).onclick = () => itp[name]())
+  (name: any) =>
+    (document.querySelector<HTMLButtonElement>(`.${name}`)!.onclick = () => {
+      // @ts-ignore
+      itp[name]()
+    })
 )
 
 const inputProgress = document.querySelector<HTMLInputElement>(".progress")
-inputProgress.onchange = () => {
-  console.log("e", parseFloat(inputProgress.value) / 100)
-  itp.seek(parseFloat(inputProgress.value) / 100)
+
+if (inputProgress) {
+  inputProgress.onchange = () => {
+    console.log("e", parseFloat(inputProgress.value) / 100)
+    itp.seek(parseFloat(inputProgress.value) / 100)
+  }
 }
+
 const $el = document.querySelector<HTMLElement>(".ball")
 const itp = new Interpol({
   props: {
@@ -18,6 +26,6 @@ const itp = new Interpol({
   duration: 1000,
   ease: "power3.out",
   onUpdate: ({ x, y }, time, progress) => {
-    $el.style.transform = `translate3d(${x}px, ${y}px, 0px)`
+    $el!.style.transform = `translate3d(${x}px, ${y}px, 0px)`
   },
 })
