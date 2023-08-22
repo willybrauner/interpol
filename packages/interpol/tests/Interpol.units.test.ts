367efd7c25a201df1811fd18689c52db0fe4770c
Willy Brauner <willybrauner@gmail.com>
2023-08-22 18:19:33 +0200

    unit test on unit prop

---
+++ packages/interpol/tests/Interpol.units.test.ts
@@ -0,0 +1,47 @@
+import { it, expect, describe, vi } from "vitest"
+import { Interpol } from "../src"
+
+describe.concurrent("Interpol units", () => {
+  it("should return a string value with unit", async () => {
+    const test = (unit) =>
+      new Promise((resolve: any) => {
+        const callback = ({ v }) => {
+          expect(v).toBeTypeOf("string")
+          expect(v).toContain(unit)
+          expect(v.slice(-unit.length)).toBe(unit)
+        }
+        new Interpol({
+          props: { v: [5, 100, unit] },
+          duration: 100,
+          beforeStart: ({ v }) => {
+            callback({ v })
+            expect(v).toBe(5 + unit)
+          },
+          onUpdate: callback,
+          onComplete: ({ v }) => {
+            callback({ v })
+            expect(v).toBe(100 + unit)
+            resolve()
+          },
+        })
+      })
+
+    return Promise.all(
+      ["px", "rem", "svh", "foo", "bar", "whatever-unit-string-we-want"].map((e) => test(e))
+    )
+  })
+
+  it("should return a number value without unit", async () => {
+    return new Promise(async (resolve: any) => {
+      const callback = ({ v }) => expect(v).toBeTypeOf("number")
+
+      new Interpol({
+        props: { v: [5, 100] },
+        duration: 100,
+        beforeStart: callback,
+        onUpdate: callback,
+        onComplete: resolve,
+      })
+    })
+  })
+})
