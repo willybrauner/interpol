7d58f63f414c6d9e5824bc949361a31261d2e67a
Willy Brauner <willybrauner@gmail.com>
2023-10-16 22:41:39 +0200

    el property should accepts object

--- packages/interpol/tests/Interpol.el.test.ts
+++ packages/interpol/tests/Interpol.el.test.ts
@@ -34,4 +34,49 @@ describe.concurrent("Interpol DOM el", () => {
       resolve()
     })
   })
+
+  it("should set prop key and value on Object element", async () => {
+    const testElObj1 = () =>
+      new Promise(async (resolve: any) => {
+        const program = { uniform: { uProgress: { value: -100 } } }
+        const callback = ({ value }) => {
+          expect(program.uniform.uProgress.value).toBe(value)
+        }
+        const itp = new Interpol({
+          el: program.uniform.uProgress,
+          duration: 100,
+          props: {
+            value: [program.uniform.uProgress.value, 100],
+          },
+          beforeStart: callback,
+          onUpdate: callback,
+          onComplete: callback,
+        })
+
+        await itp.play()
+        resolve()
+      })
+
+    const testElObj2 = () =>
+      new Promise(async (resolve: any) => {
+        const program = { v: 0 }
+        const callback = ({ v }) => {
+          expect(program.v).toBe(v)
+        }
+        const itp = new Interpol({
+          el: program,
+          duration: 300,
+          props: {
+            v: [program.v, 1000],
+          },
+          beforeStart: callback,
+          onUpdate: callback,
+          onComplete: callback,
+        })
+        await itp.play()
+        resolve()
+      })
+
+    return Promise.all([testElObj1(), testElObj2()])
+  })
 })
