a47a4320226cfb392df1494307e45218d4e1b142
Willy Brauner <willybrauner@gmail.com>
2024-02-21 08:35:05 +0100

    update options

--- packages/interpol/tests/options.test.ts
+++ packages/interpol/tests/options.test.ts
@@ -1,33 +1,8 @@
-import { describe, expect, it, vi } from "vitest"
-import { Interpol, InterpolOptions, Ticker } from "../src"
-import { wait } from "./utils/wait"
+import { describe, expect, it } from "vitest"
+import { InterpolOptions, Ticker } from "../src"
 
 describe.concurrent("options", () => {
   it("options should expose Ticker instance", () => {
     expect(InterpolOptions.ticker).toBeInstanceOf(Ticker)
-  })
-
-  it("options should expose duration and set affect all itps", () => {
-    const newDuration = 100
-    InterpolOptions.duration = newDuration
-
-    const mock = vi.fn()
-    return new Promise(async (resolve: any) => {
-      const itp = new Interpol({
-        paused: true,
-        props: { v: [-100, 100] },
-        onComplete: mock,
-      })
-      expect(itp.duration).toBe(newDuration)
-
-      // second test, play itp
-      itp.play()
-      const offset = 10
-      await wait(newDuration - offset)
-      expect(mock).toHaveBeenCalledTimes(0)
-      await wait(offset + 10)
-      expect(mock).toHaveBeenCalledTimes(1)
-      resolve()
-    })
   })
 })
