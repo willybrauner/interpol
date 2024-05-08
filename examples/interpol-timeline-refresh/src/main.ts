99120d658c729a5207ad2b92e6feee7cc12f4784
Willy Brauner <willybrauner@gmail.com>
2024-05-08 06:53:07 +0200

    Update example

--- examples/interpol-timeline-refresh/src/main.ts
+++ examples/interpol-timeline-refresh/src/main.ts
@@ -1,16 +1,8 @@
 import { Timeline } from "@wbe/interpol"
 import "./index.less"
 
-/**
- * Query
- */
 const ball = document.querySelector<HTMLElement>(".ball")
-
-
-/**
- * Timeline
- */
-const tl: Timeline = new Timeline({ debug: false, paused: true})
+const tl: Timeline = new Timeline({ debug: false, paused: true })
 
 /**
  * The goal of this example is to use an external value, muted on onUpdate callbacks
@@ -21,47 +13,39 @@ const tl: Timeline = new Timeline({ debug: false, paused: true})
  */
 let EXTERNAL_X = 0
 
-tl.add(
-  {
-    el: ball,
-    duration: 1000,
-    ease: "power3.in",
-    props: {
-      x: [0, 200, "px"],
-    },
-    onUpdate: ({ x, y }) => {
-      // register the external value
-      EXTERNAL_X = parseFloat(x)
-      console.log(EXTERNAL_X)
-    }
-  }
-)
+tl.add({
+  el: ball,
+  duration: 1000,
+  ease: "power3.in",
+  props: {
+    x: [0, 70, "vw"],
+  },
+  onUpdate: ({ x }) => {
+    EXTERNAL_X = parseFloat(x)
+    console.log("1 - x", x)
+  },
+})
 tl.add({
   el: ball,
   duration: 1000,
   props: {
-    x: [ ()=> EXTERNAL_X, 50, "px"],
+    x: [() => EXTERNAL_X, 20, "vw"],
   },
-  onUpdate: ({ x, y }) => {
-    // register the external value
+  onUpdate: ({ x }) => {
     EXTERNAL_X = parseFloat(x)
     console.log("2 - x", x)
-  }
+  },
 })
 
 tl.add({
   el: ball,
   duration: 1000,
   props: {
-    x: [ ()=> EXTERNAL_X, 100, "px"],
+    x: [() => EXTERNAL_X, 50, "vw"],
   },
-  onUpdate: ({ x, y }) => {
+  onUpdate: ({ x }) => {
     console.log("3 - x", x)
-  }
+  },
 })
 
 tl.play()
-//   .then(() => {
-//   tl.reverse()
-// })
-
