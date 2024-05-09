64ba68f71721063988f69af80a841671aad641e1
Willy Brauner <willybrauner@gmail.com>
2024-05-09 11:56:18 +0200

    feat: Add test

--- examples/interpol-seek-reset-wall/src/main.ts
+++ examples/interpol-seek-reset-wall/src/main.ts
@@ -4,46 +4,88 @@ import "./style.css"
 const wall = document.querySelector<HTMLElement>(".wall")
 const button = document.querySelector<HTMLElement>(".button")
 
-const tl = new Timeline({ paused: true })
+/**
+ * Test wall seek played on click & seeked on resize
+ * Goal: the wall should seek properly and computedValues refreshed when the window is resized
+ */
 
-const itp = new Interpol({
-  paused: true,
-  debug: true,
-  el: wall,
-  immediateRender: true,
-  duration: 1000,
-  ease: "linear",
-  props: {
-    x: [() => -innerWidth * 0.9, 0, "px"],
-  },
-})
+/**
+ * Test with Interpol
+ */
+const testWithInterpol = () => {
+  const itp = new Interpol({
+    immediateRender: true,
+    paused: true,
+    debug: true,
+    el: wall,
+    duration: 1000,
+    ease: "linear",
+    props: {
+      x: [() => -innerWidth * 0.9, 0, "px"],
+    },
+  })
 
-tl.add(itp)
-tl.add({
-  debug: true,
-  el: wall,
-  duration: 1000,
-  ease: "linear",
-  props: {
-    x: [0, () => -innerWidth * 0.5, "px"],
-  },
-})
-
-let isVisible = false
-
-const openClose = () => {
-  isVisible = !isVisible
-  if (isVisible) tl.play()
-  else tl.reverse()
-}
-openClose()
-button?.addEventListener("click", () => {
+  let isVisible = false
+  const openClose = () => {
+    isVisible = !isVisible
+    if (isVisible) itp.play()
+    else itp.reverse()
+  }
   openClose()
-})
+  button?.addEventListener("click", () => {
+    openClose()
+  })
 
-window.addEventListener("resize", () => {
-  tl.refreshComputedValues()
-  tl.seek(0)
+  window.addEventListener("resize", () => {
+    itp.refreshComputedValues()
+    itp.seek(0)
+    isVisible = false
+  })
+}
 
-  isVisible = false
-})
+testWithInterpol()
+
+/**
+ * Test with Timeline
+ */
+const testWithTimeline = () => {
+  const tl = new Timeline({ paused: true })
+  tl.add({
+    immediateRender: true,
+    paused: true,
+    debug: true,
+    el: wall,
+    duration: 1000,
+    ease: "linear",
+    props: {
+      x: [() => -innerWidth * 0.9, 0, "px"],
+    },
+  })
+  tl.add({
+    debug: true,
+    el: wall,
+    duration: 1000,
+    ease: "linear",
+    props: {
+      x: [0, () => -innerWidth * 0.5, "px"],
+    },
+  })
+
+  let isVisible = false
+  const openClose = () => {
+    isVisible = !isVisible
+    if (isVisible) tl.play()
+    else tl.reverse()
+  }
+  openClose()
+  button?.addEventListener("click", () => {
+    openClose()
+  })
+  window.addEventListener("resize", () => {
+    tl.refreshComputedValues()
+    tl.seek(0)
+    isVisible = false
+  })
+}
+
+// testWithTimeline()
