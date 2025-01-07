2ab2390163b1412b1111cdec84a15dc3e8ac67ff
Willy Brauner <willybrauner@gmail.com>
2025-01-08 00:22:28 +0100

    fix: example interpol-timeline

--- examples/interpol-timeline/src/main.ts
+++ examples/interpol-timeline/src/main.ts
@@ -1,4 +1,4 @@
-import { Power1, Timeline, Interpol } from "@wbe/interpol"
+import { Power1, Timeline, Interpol, styles } from "@wbe/interpol"
 import "./index.less"
 
 /**
@@ -6,11 +6,9 @@ import "./index.less"
  */
 const ball = document.querySelector<HTMLElement>(".ball")
 const ball2 = document.querySelector<HTMLElement>(".ball-2")
-
 const seek0 = document.querySelector<HTMLButtonElement>(".seek-0")
 const seek05 = document.querySelector<HTMLButtonElement>(".seek-05")
 const seek1 = document.querySelector<HTMLButtonElement>(".seek-1")
-
 const inputProgress = document.querySelector<HTMLInputElement>(".progress")
 const inputSlider = document.querySelector<HTMLInputElement>(".slider")
 
@@ -23,10 +21,8 @@ const inputSlider = document.querySelector<HTMLInputElement>(".slider")
 seek0.onclick = () => tl.seek(0, false, false)
 seek05.onclick = () => tl.seek(0.5, false, false)
 seek1.onclick = () => tl.seek(1, false, false)
-
 inputProgress.onchange = () => tl.seek(parseFloat(inputProgress.value) / 100, false, false)
 inputSlider.oninput = () => tl.seek(parseFloat(inputSlider.value) / 100, false, false)
-
 window.addEventListener("resize", () => tl.seek(1))
 
 /**
@@ -38,12 +34,12 @@ const tl: Timeline = new Timeline({
 })
 
 const itp = new Interpol({
-  el: ball,
-  props: {
-    x: [0, 200, "px"],
-    y: [0, 200, "px"],
-  },
+  x: [0, 200],
+  y: [0, 200],
   ease: Power1.in,
+  onUpdate: ({ x, y }) => {
+    styles(ball, { x: x + "px", y: y + "px" })
+  },
   onComplete: (e) => {
     console.log("itp 1 onComplete", e)
   },
@@ -51,24 +47,24 @@ const itp = new Interpol({
 tl.add(itp)
 
 tl.add({
-  el: ball,
-  props: {
-    x: [200, 100, "px"],
-    y: [200, 300, "px"],
-  },
+  x: [200, 100],
+  y: [200, 300],
   ease: Power1.out,
+  onUpdate: ({ x, y }) => {
+    styles(ball, { x: x + "px", y: y + "px" })
+  },
   onComplete: (e) => {
     console.log("itp 2 onComplete", e)
   },
 })
 
 tl.add({
-  el: ball2,
-  props: {
-    x: [0, 100, "px"],
-    y: [0, 400, "px"],
-  },
+  x: [0, 100],
+  y: [0, 400],
   ease: Power1.out,
+  onUpdate: ({ x, y }) => {
+    styles(ball2, { x: x + "px", y: y + "px" })
+  },
   onComplete: (e) => {
     console.log("itp 3 onComplete", e)
   },
