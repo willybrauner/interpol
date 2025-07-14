bdffce7b5a3a8c9b80163059bb3234fd67bb6667
Willy Brauner <willybrauner@gmail.com>
2025-07-14 23:26:50 +0200

    feat: Install astro site documentation

---
+++ documentation/astro.config.mjs
@@ -0,0 +1,28 @@
+// @ts-check
+import { defineConfig } from "astro/config"
+import starlight from "@astrojs/starlight"
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "interpol",
+      social: [
+        { icon: "github", label: "GitHub", href: "https://github.com/willybrauner/interpol" },
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
+})
