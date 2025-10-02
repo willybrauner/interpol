3fd0381a917ee518dd60e310c23f26011b9a7c47
Willy Brauner <willybrauner@gmail.com>
2025-10-02 18:29:19 +0200

    Update example interpol-basic

--- examples/interpol-basic/src/main.ts
+++ examples/interpol-basic/src/main.ts
@@ -1,4 +1,4 @@
-import "./index.css"
+import "./styles.css"
 import { EaseName, Interpol, styles } from "@wbe/interpol"
 import { Pane } from "tweakpane"
 
@@ -7,7 +7,7 @@ const element = document.querySelector<HTMLElement>(".element")!
 const PARAMS = {
   ease: "power3.out" as EaseName,
   duration: 2000,
-  x: 50,
+  x: -100,
   scale: 1.4,
 }
 
@@ -21,7 +21,10 @@ const itp = new Interpol({
   onUpdate: ({ x, rotate, scale }) => styles(element, { x, rotate, scale }),
 })
 
-const pane = new Pane()
+const pane = new Pane({
+  title: "Controls",
+  expanded: true,
+})
 pane.addButton({ title: "play" }).on("click", () => itp.play())
 pane.addButton({ title: "reverse" }).on("click", () => itp.reverse())
 pane.addButton({ title: "pause" }).on("click", () => itp.pause())
@@ -29,18 +32,19 @@ pane.addButton({ title: "stop" }).on("click", () => itp.stop())
 pane.addButton({ title: "resume" }).on("click", () => itp.resume())
 pane.addButton({ title: "refresh" }).on("click", () => itp.refresh())
 
-// add x binding
 pane.addBinding(PARAMS, "x", { min: -200, max: 200, label: "x (to)" }).on("change", () => {
   itp.refresh()
 })
-
-// add scale binding
 pane
-  .addBinding(PARAMS, "scale", { min: 0.7, max: 2.5, step: 0.1, label: "scale (to)" })
+  .addBinding(PARAMS, "scale", {
+    min: 0.7,
+    max: 2.5,
+    step: 0.1,
+    label: "scale (to)",
+  })
   .on("change", () => {
     itp.refresh()
   })
-
 pane.addBinding({ progress: itp.progress() }, "progress", { min: 0, max: 1 }).on("change", (ev) => {
   itp.progress(ev?.value || 0)
 })
