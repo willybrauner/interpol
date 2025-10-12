eaacd30a4f71377e77dfb5584378a7f2be372091
Willy Brauner <willybrauner@gmail.com>
2025-10-12 23:25:19 +0200

    update interpol gl property

---
+++ examples/interpol-gl-property/src/index.ts
@@ -0,0 +1,76 @@
+import "./index.css"
+import { EaseName, Interpol, InterpolOptions } from "@wbe/interpol"
+import { Renderer, Program, Mesh, Transform, Geometry } from "ogl"
+import { createTweekpane } from "./utils/createTweakpane"
+
+const renderer = new Renderer({ width: window.innerWidth, height: window.innerHeight })
+const gl = renderer.gl
+document.body.appendChild(gl.canvas)
+
+window.addEventListener("resize", () => {
+  renderer.setSize(window.innerWidth, window.innerHeight)
+})
+
+const scene = new Transform()
+
+const program = new Program(gl, {
+  vertex: /* glsl */ `
+      attribute vec2 uv;
+      attribute vec2 position;
+      varying vec2 vUv;
+      void main() {
+          vUv = uv;
+          gl_Position = vec4(position, 0, 1);
+      }
+    `,
+  fragment: /* glsl */ `
+      precision highp float;
+      varying vec2 vUv;
+      uniform float uMove;
+      void main() {
+          float strength = floor(vUv.y * 15. * uMove) / 15.;
+          gl_FragColor = vec4(vec3(strength), 1.);
+      }
+    `,
+  transparent: true,
+  uniforms: {
+    uMove: { value: 0 },
+  },
+})
+
+const geometry = new Geometry(gl, {
+  position: { size: 2, data: new Float32Array([-1, -1, 3, -1, -1, 3]) },
+  uv: { size: 2, data: new Float32Array([0, 0, 2, 0, 0, 2]) },
+})
+
+const mesh = new Mesh(gl, { geometry, program })
+mesh.setParent(scene)
+
+InterpolOptions.ticker.add(() => {
+  renderer.render({ scene })
+})
+
+const PARAMS = {
+  duration: 1000,
+  ease: "power2.out",
+}
+
+/**
+ * Interpol
+ */
+const itp = new Interpol({
+  value: [0, 1],
+  ease: () => PARAMS.ease as EaseName,
+  duration: () => PARAMS.duration,
+  onUpdate: ({ value }) => {
+    program.uniforms.uMove.value = value
+  },
+})
+
+const yoyo = async () => {
+  await itp.play()
+  await itp.reverse()
+  yoyo()
+}
+yoyo()
+createTweekpane(itp, PARAMS, yoyo)
--- examples/interpol-object-el/src/shaders/fragment.glsl
+++
@@ -1,8 +0,0 @@
-precision highp float;
-varying vec2 vUv;
-uniform float uMove;
-
-void main() {
-    float strength = floor(vUv.y * 10. * uMove) / 10.;
-    gl_FragColor = vec4(vec3(strength), 1.);
-}
